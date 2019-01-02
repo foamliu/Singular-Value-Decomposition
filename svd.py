3fd7a058cd53d7fe1f5444d086c277dc7ffc7845
Yang Liu <liuyang12@focusmedia.cn>
2019-01-02 10:24:46 +0800

    update

--- svd.py
+++ svd.py
@@ -1,15 +1,13 @@
 import cv2
 import numpy as np
 
-num_sv = 100
-
 
 def reconstruct(img):
     u, s, vh = np.linalg.svd(img)
-    e = np.eye(height, weight)
-    d = np.zeros(s.shape)
-    d[0:num_sv] = s[0:num_sv]
-    s = e * d
+    d = np.zeros((height, weight))
+    for i in range(0, num_sv):
+        d[i, i] = s[i]
+    s = d
 
     ret = np.matmul(np.matmul(u, s), vh)
     val_min = np.min(ret)
@@ -21,24 +19,26 @@ def reconstruct(img):
 
 if __name__ == '__main__':
     image = cv2.imread('image/sophie_marceau.jpg')
+    # image = cv2.imread('image/fun.jpeg')
 
     height, weight = image.shape[:2]
     b = image[:, :, 0]
     g = image[:, :, 1]
     r = image[:, :, 2]
 
-    b_re = reconstruct(b)
-    g_re = reconstruct(g)
-    r_re = reconstruct(r)
+    for num_sv in [1, 5, 20, 50, 100]:
+        b_re = reconstruct(b)
+        g_re = reconstruct(g)
+        r_re = reconstruct(r)
 
-    vis = np.zeros((height, weight, 3), np.uint8)
+        vis = np.zeros((height, weight, 3), np.uint8)
 
-    vis[:, :, 0] = b_re
-    vis[:, :, 1] = g_re
-    vis[:, :, 2] = r_re
+        vis[:, :, 0] = b_re
+        vis[:, :, 1] = g_re
+        vis[:, :, 2] = r_re
 
-    cv2.imshow('old', image)
-    cv2.imshow('svd', vis)
-    cv2.imwrite('image/sv-{}.png'.format(num_sv), vis)
-    cv2.waitKey(0)
+        # cv2.imshow('old', image)
+        # cv2.imshow('svd', vis)
+        cv2.imwrite('image/sv-{}.png'.format(num_sv), vis)
+        # cv2.waitKey(0)
     cv2.destroyAllWindows()
